fead6d274e5b122689aa002d9c5360f83e951b89
sorinacostache06 <sorinacostache06@yahoo.com>
2017-04-05 15:39:04 +0300

    crawler working

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -25,23 +25,36 @@ class HomeController extends Controller
 
     public function crowAction(Request $request)
     {
-        $this->getAllDistinctLinks('http://www.cinemagia.ro/');
-//        for ($i=0; $i<$this->countLinks; $i++) {
-//            echo $this->arrayLinks[$i] . "<br/>";
-//        }
         $em = $this->getDoctrine()->getManager();
         $repo = $em->getRepository('AppBundle:Test');
         $qb = $em->createQueryBuilder();
         $access_repo = $repo->selectAll($qb);
-        $result = $access_repo->getQuery()->getResult();
+        $results = $access_repo->getQuery()->getResult();
 
-//        var_dump($result[100]->getLink());
-          // pt fiecare link din test fac getAllDistinctLinks
+        $this->getMovieFromLinks($results);
+
         return new Response('Welcome!');
+    }
+
+    public function startCrawlerAction()
+    {
+        $this->getAllDistinctLinks('http://www.cinemagia.ro/');
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:Test');
+        $qb = $em->createQueryBuilder();
+        $access_repo = $repo->selectAll($qb);
+        $results = $access_repo->getQuery()->getResult();
+
+        set_time_limit (300);
+        foreach ($results as $result) {
+            $url = $result->getLink();
+            $this->getAllDistinctLinks($url);
+        }
     }
 
     public function getAllDistinctLinks($siteUrl)
     {
+
         $client = new Client();
         $crawler = new Crawler();
         $crawler = $client->request('GET', $siteUrl);
@@ -49,24 +62,27 @@ class HomeController extends Controller
         $links = $crawler->filter('a')->links();
         foreach ($links as $link) {
             $l = $link->getUri();
-            $em = $this->getDoctrine()->getManager();
-            $repo = $em->getRepository('AppBundle:Test');
-            $qb = $em->createQueryBuilder();
-            $access_repo = $repo->distinctLink($qb, $l);
-            $result = $access_repo->getQuery()->getResult();
-            if ($result == NULL) {
-                $this->arrayLinks[$this->countLinks++] = $l;
-                $test = new Test();
-                $test->setLink($l);
-                $em->persist($test);
-                $em->flush();
+            $url = parse_url($l);
+            $host = (isset($url['host'])) ? $url['host'] : '';
+            if (strcmp($host,"www.cinemagia.ro") == 0) {
+                $em = $this->getDoctrine()->getManager();
+                $repo = $em->getRepository('AppBundle:Test');
+                $qb = $em->createQueryBuilder();
+                $access_repo = $repo->distinctLink($qb, $l);
+                $result = $access_repo->getQuery()->getResult();
+                if ($result == NULL) {
+                    $this->arrayLinks[$this->countLinks++] = $l;
+                    $test = new Test();
+                    $test->setLink($l);
+                    $em->persist($test);
+                    $em->flush();
+                }
             }
         }
-
     }
 
 
-    public function getMovieFromLinks($request_url)
+    public function getMovieFromLinks($links)
     {
         //get year and get name
 //        $titles_crowler = $crawler->filter('h1 > a');
@@ -77,13 +93,8 @@ class HomeController extends Controller
 //
 //                        echo $l . " " . $year . " " . $title . "<br/>";
 
-        $client = new Client();
-        $crawler = new Crawler();
-        $crawler = $client->request('GET', $request_url);
-
-        $links = $crawler->filter('a')->links();
         foreach ($links as $link) {
-            $l = $link->getUri();
+            $l = $link->getLink();
             $url = parse_url($l);
             $host = (isset($url['host'])) ? $url['host'] : '';
             $path_url = (isset($url['path'])) ? $url['path'] : '';
@@ -92,18 +103,18 @@ class HomeController extends Controller
                 if (count($path) == 4 and strcmp($path[1],"filme") == 0 and strcmp($path[3],"") == 0) {
                     $name = explode('-',$path[2]);
                     if (is_numeric($name[count($name) -1])){
-                        echo $l . "<br/>";
-                        $em = $this->getDoctrine()->getManager();
-                        $repo = $em->getRepository('AppBundle:Test');
-                        $qb = $em->createQueryBuilder();
-                        $access_repo = $repo->distinctLink($qb, $l);
-                        $result = $access_repo->getQuery()->getResult();
-                        if ($result == NULL) {
-                            $test = new Test();
-                            $test->setLink($l);
-                            $em->persist($test);
-                            $em->flush();
-                        }
+                        echo $l . '    ' . count($path) . "<br/>";
+//                        $em = $this->getDoctrine()->getManager();
+//                        $repo = $em->getRepository('AppBundle:Test');
+//                        $qb = $em->createQueryBuilder();
+//                        $access_repo = $repo->distinctLink($qb, $l);
+//                        $result = $access_repo->getQuery()->getResult();
+//                        if ($result == NULL) {
+//                            $test = new Test();
+//                            $test->setLink($l);
+//                            $em->persist($test);
+//                            $em->flush();
+//                        }
                     }
                 }
             }
